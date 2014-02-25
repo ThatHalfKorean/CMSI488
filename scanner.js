c6bde0a87aebd40532442bb45e918104118bd95d
JamesEHo <ho.james.ed@gmail.com>
2014-02-24 20:23:24 -0800

    ENT AND FLOATIE NOT WERD ANYMUR NOW IT BE uh.. NOM

--- scanner.js
+++ scanner.js
@@ -59,7 +59,7 @@ function scan(line, linenumber, tokens) {
     } else if (/[A-Za-z]/.test(line[pos])) {
       while (/\w/.test(line[pos]) && pos < line.length) pos++
       var word = line.substring(start, pos)
-      if (/^(?:ent|buul|floatie|werd|dile|eef|tru|nuhuh|nawt|derp|tri|ketch|elsheef|elsh|herez|fer|dur|urp|pront|thang|izz|izznt)$/.test(word)) {
+      if (/^(?:nom|buul|werd|dile|eef|tru|nuhuh|nawt|derp|tri|ketch|elsheef|elsh|herez|fer|dur|urp|pront|thang|izz|izznt)$/.test(word)) {
         emit(word)
       } else {
         emit('ID', word)
