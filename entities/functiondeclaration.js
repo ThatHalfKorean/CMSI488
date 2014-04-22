00e68e8cd48be545cfd08d66806c60535ec167db
hcyr <haley.a.cyr@gmail.com>
2014-04-21 20:22:43 -0700

    functiondeclaration, needs work

--- entities/functiondeclaration.js
+++ entities/functiondeclaration.js
@@ -2,13 +2,22 @@ var Type = require('./type')
 
 function FunctionDeclaration(id, type, parameters, body) {
   this.id = id
+  this.type = type
   this.parameters = parameters
   this.body = body
 }
 
 FunctionDeclaration.prototype.toString = function () {
-  return '(Function ' + this.id + this.parameters.join(', ') +'  '+ this.body ')'
+  return '(' +  this.type + ' ' + this.id + this.parameters.join(', ') +
+         ' '+ this.body ')'
 }
 
+FunctionDeclaration.prototype.analyze = function(context) {
+  this.parameters.analyze(context)
+  this.body.analyze(context)
+
+  // HC I think something else needs to go here, but I'm not sure what yet. 
+  // Must look into later. 
+}
 
 module.exports = FunctionDeclaration
