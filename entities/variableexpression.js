669a78567955c6dd79b33f04f51da81ec55ea5de
JamesEHo <ho.james.ed@gmail.com>
2014-04-21 19:13:31 -0700

    GOT RID OF USELESS THANG

--- entities/variableexpression.js
+++
@@ -1,9 +0,0 @@
-function VariableExpression(id) {
-  this.id = id
-}
-
-VariableExpression.prototype.toString = function () {
-  return '( VarExp' + this.id+ ')'
-}
-
-module.exports = VariableExpression
