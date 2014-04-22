5f74dba81ece9360a8d7e0111d08df60c2b18f49
hcyr <haley.a.cyr@gmail.com>
2014-04-21 18:31:12 -0700

    modified call statement, still needs work

--- entities/callstatement.js
+++ entities/callstatement.js
@@ -4,14 +4,15 @@ function CallStatement(target, expressions) {
 }
 
 CallStatement.prototype.toString = function () {
-  return '( Call ' +this.target +' '+ this.expressions.join(' ') + ')'
+  return '( Call ' + this.target + ' ' + this.expressions.join(' ') + ')'
 }
 
-// CallStatement.prototype.analyze = function (context) {
-  // this.expressions.forEach(function (e) {
-    // e.analyze(context)
-    // e.type.mustBeInteger('Expressions in "write" statement must have type integer')
-  // })
-// }
+CallStatement.prototype.analyze = function (context) {
+  this.target.analyze(context)
+  this.expressions.forEach(function (e) {
+    e.analyze(context)
+    e.type.mustBeInteger('Expressions in "call" statement must have type integer')
+  })
+}
 
 module.exports = CallStatement
