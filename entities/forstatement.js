eed0541c6094bf67ed3a886a7d9d9c0d45f781ad
JamesEHo <ho.james.ed@gmail.com>
2014-04-29 10:19:30 -0700

    fer derp

--- entities/forstatement.js
+++ entities/forstatement.js
@@ -1,19 +1,15 @@
-function ForStatement(declaration, condition, assignment, body) {
-  this.declaration = declaration
-  this.condition = condition
-  this.assignment = assignment
+function ForStatement(conditions, body) {
+  this.conditions = conditions
   this.body = body 
 }
 
 ForStatement.prototype.toString = function () {
-  return '(Fer ' + this.declaration + 'derp' + this.condition + 
-  		  'derp' + this.assignment + ' ' + this.body + ')'
+  return '((Conditions ' + this.conditions + 
+  		  ')(Body ' + this.body + '))'
 }
 
 ForStatement.prototype.analyze = function (context) {
-  this.declaration.analyze(context)
-  this.condition.analyze(context)
-  this.assignment.analyze(context)
+  this.conditions.analyze(context)
   this.body.analyze(context)
 }
 
