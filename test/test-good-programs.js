b986b7ed6b267e6da9b27bd13683f8db3d85bbbc
ThatHalfKorean <gkim6@lion.lmu.edu>
2014-05-01 11:24:41 -0700

    fixed some tests

--- test/test-good-programs.js
+++ test/test-good-programs.js
@@ -1,1 +1,21 @@
-// TODO: Test to make sure all files in the good-programs directory pass
+var fs = require('fs')
+var path = require('path')
+var should = require('should')
+var scan = require('../scanner')
+var parse = require('../parser')
+var error = require('../error')
+
+var TEST_DIR = 'test/data/good-programs'
+
+describe('The compiler', function () {
+  fs.readdirSync(TEST_DIR).forEach(function (name) {
+    it('should compile ' + name + ' without errors', function (done) {
+      scan(path.join(TEST_DIR, name), function (tokens) {
+        var priorErrorCount = error.count
+        parse(tokens).analyze()
+        error.count.should.eql(priorErrorCount)
+        done()
+      })
+    })
+  })
+})
