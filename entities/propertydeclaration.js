7d0b9537aa453b28ff1677382502fbbc9a0461a5
JamesEHo <ho.james.ed@gmail.com>
2014-05-07 16:33:30 -0700

    Fixed type check in property dec

--- entities/propertydeclaration.js
+++ entities/propertydeclaration.js
@@ -1,3 +1,6 @@
+var util = require('util')
+var Type = require('./type')
+
 function PropertyDeclaration(id, type, value) {
   this.id = id
   this.type = type
@@ -11,6 +14,9 @@ PropertyDeclaration.prototype.toString = function () {
 PropertyDeclaration.prototype.analyze = function (context) {
   context.variableMustNotBeAlreadyDeclared(this.id)
   context.addVariable(this.id.lexeme, this)
+  this.value.analyze(context)
+  this.value.type.mustBeCompatibleWith(this.type, 
+  util.format('Type mismatch in assignment: %j = %j', this.value.type, this.type))
 }
 
 module.exports = PropertyDeclaration
